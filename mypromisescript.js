ee8d3e9a3d8f31f2ce9e67e263e80a43a36ac326
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 15:23:03 -0600

    Update mypromisescript.js

--- mypromisescript.js
+++ mypromisescript.js
@@ -1,16 +1,24 @@
-let myPromise1 = new Promise((resolve,reject) => {
+// Create Promise 1
+let myPromise1 = new Promise((resolve, reject) => {
+    // Resolve after 6 seconds
     setTimeout(() => {
-      resolve("Promise 1 resolved")
-    },6000)})
+        resolve("Promise 1 resolved");
+    }, 6000);
+});
 
-let myPromise2 = new Promise((resolve,reject) => {
+// Create Promise 2
+let myPromise2 = new Promise((resolve, reject) => {
+    // Resolve after 3 seconds
     setTimeout(() => {
-        resolve("Promise 2 resolved")
-    },3000)})
+        resolve("Promise 2 resolved");
+    }, 3000);
+});
 
+// Handle Promise 1
 myPromise1.then((successMessage) => {
-    console.log("From Callback " + successMessage)
+    console.log("From Callback " + successMessage);
+    // Chain Promise 2 inside Promise 1 callback
     myPromise2.then((successMessage) => {
-        console.log("From Callback " + successMessage)
-    })
-    })
+        console.log("From Callback " + successMessage);
+    });
+});

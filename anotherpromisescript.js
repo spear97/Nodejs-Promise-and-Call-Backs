24e3491de11904826744057af1637f9726de80c6
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 15:22:38 -0600

    Update anotherpromisescript.js

--- anotherpromisescript.js
+++ anotherpromisescript.js
@@ -1,17 +1,25 @@
-let myPromise1 = new Promise((resolve,reject) => {
+// Create Promise 1
+let myPromise1 = new Promise((resolve, reject) => {
+    // Resolve after 6 seconds
     setTimeout(() => {
-      resolve("Promise 1 resolved")
-    },6000)})
+        resolve("Promise 1 resolved");
+    }, 6000);
+});
 
-let myPromise2 = new Promise((resolve,reject) => {
+// Create Promise 2
+let myPromise2 = new Promise((resolve, reject) => {
+    // Resolve after 3 seconds
     setTimeout(() => {
-      resolve("Promise 2 resolved")
-    },3000)})
+        resolve("Promise 2 resolved");
+    }, 3000);
+});
 
-  myPromise1.then((successMessage) => {
-    console.log("From Callback " + successMessage)
-  })
+// Handle Promise 1
+myPromise1.then((successMessage) => {
+    console.log("From Callback " + successMessage);
+});
 
-  myPromise2.then((successMessage) => {
-  console.log("From Callback " + successMessage)
-})
+// Handle Promise 2
+myPromise2.then((successMessage) => {
+    console.log("From Callback " + successMessage);
+});
